ea4b30271c3cfdc235e6e0a8012f1516c7d3f71e
krishnan2098 <krishnan2098@gmail.com>
2017-12-13 12:30:06 +0530

    Implemented Directives

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,6 +11,8 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'Testserver';
+  serverCreated = false;
+  servers = ['Testserver','Testserver 2'];
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -19,7 +21,10 @@ export class ServersComponent implements OnInit {
 
   ngOnInit() {
   }
+
   onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
   onUpdateServerName(event: Event) {
